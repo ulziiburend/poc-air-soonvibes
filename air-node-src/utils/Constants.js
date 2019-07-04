f71486172196e729b18048e9ff700e230d70edcb
Ulziiburen <ulziiburend@gmail.com>
2019-07-05 00:06:07 +0200

    node FLV url

--- air-node-src/utils/Constants.js
+++ air-node-src/utils/Constants.js
@@ -1,7 +1,7 @@
 
 const CHANNELS_PLAYLIST_DIR = 'channels-stream-playlist/';
 const FFMPEG_FILE_REG = "file '";
-const FLV_URL = 'http://localhost:8000/live/channel-';
+const FLV_URL = 'http://ec2-13-125-90-167.ap-northeast-2.compute.amazonaws.com:8000/live/channel-';
 const FLV_FILE_EXT = '.flv';
 const SERVER_DELAY = 4;
 
