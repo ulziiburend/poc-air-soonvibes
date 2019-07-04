9187e1b4fc5ccc493b252c1c7e811b068a10c24c
Ulziiburen <ulziiburend@gmail.com>
2019-07-04 23:58:36 +0200

    react socket

--- air-react-src/src/socket-client/Constants.js
+++ air-react-src/src/socket-client/Constants.js
@@ -1,3 +1,3 @@
-export const MAIN_URL = "http://172.31.22.122:9000"
+export const MAIN_URL = "http://ec2-13-125-90-167.ap-northeast-2.compute.amazonaws.com:9000"
 export const CHANNEL_NAMESPACE = "/channel-"
 export const CHAT_NAMESPACE = "/chat"
